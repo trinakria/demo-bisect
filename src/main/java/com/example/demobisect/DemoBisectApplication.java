a386e69508726ae350d57ff93c9767f7eabb9844
GIURLANDA Mario <mario.giurlanda@hexagon.com>
2023-05-30 16:59:35 +0200

    first commit

---
+++ src/main/java/com/example/demobisect/DemoBisectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobisect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBisectApplication {
+
+  public static void main( String[] args ) {
+    SpringApplication.run( DemoBisectApplication.class, args );
+  }
+
+}
---
+++ src/test/java/com/example/demobisect/DemoBisectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobisect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBisectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
